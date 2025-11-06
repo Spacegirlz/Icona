b5a0fa59faa30bcd1dfa668ab71b4eb186784433
Piet Marie <pietmarie@Piets-Mega-Macbook-Pro.local>
2025-11-06 22:24:51 +1100

    Remove unused useEffect import

--- components/CreditBalance.tsx
+++ components/CreditBalance.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { createCheckoutSession } from '../services/stripeService';
 
 interface CreditBalanceProps {
